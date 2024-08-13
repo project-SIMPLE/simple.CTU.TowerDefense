c11d30f6d2c3a2d710e724423d6b0462f4eed470
hqnghi88 <hqnghi88@gmail.com>
2024-08-13 23:07:18 +0700

    retry multi player

--- Assets/Scripts/Gama Provider/Simulation/SimulationManagerMulti.cs
+++ Assets/Scripts/Gama Provider/Simulation/SimulationManagerMulti.cs
@@ -18,14 +18,22 @@ public class SimulationManagerMulti : SimulationManager
 
     protected override void ManageOtherInformation()
     {
-        
+        string id = ConnectionManager.Instance.getUseMiddleware() ? ConnectionManager.Instance.GetConnectionId() : ("\"" + ConnectionManager.Instance.GetConnectionId() + "\"");
+         int index  = infoWorld.players.IndexOf(id);
+        ranking = infoWorld.ranking[index];
+        numTokens = infoWorld.numTokens;
+        updateHUD();
 
 
     }
 
     protected void updateHUD()
     {
-     
+        if (infoPlayerHUD == null)
+            infoPlayerHUD = GameObject.FindGameObjectWithTag("HUD").GetComponentInChildren<TextMeshProUGUI>();
+
+        if (infoPlayerHUD != null)
+            infoPlayerHUD.SetText("Score: " + score + " - Ranking: " + ranking +  " - Token to Find: " + numTokens);
     }
 
     protected override void TriggerMainButton()
@@ -43,4 +51,39 @@ public class SimulationManagerMulti : SimulationManager
 
     }
 
+    protected override void HoverEnterInteraction(HoverEnterEventArgs ev)
+    {
+
+        GameObject obj = ev.interactableObject.transform.gameObject;
+        ChangeColor(obj, Color.blue);
+    }
+
+    protected override void HoverExitInteraction(HoverExitEventArgs ev)
+    {
+        GameObject obj = ev.interactableObject.transform.gameObject;
+        ChangeColor(obj, Color.white);
+
+    }
+
+    protected override void SelectInteraction(SelectEnterEventArgs ev)
+    {
+
+        if (remainingTime <= 0.0)
+        {
+            GameObject grabbedObject = ev.interactableObject.transform.gameObject;
+
+            string id = ConnectionManager.Instance.getUseMiddleware() ? ConnectionManager.Instance.GetConnectionId() : ("\"" + ConnectionManager.Instance.GetConnectionId() + "\"");
+
+            Dictionary<string, string> args = new Dictionary<string, string> {
+                         {"id", grabbedObject.name },
+                         {"player", id }
+                    };
+                ConnectionManager.Instance.SendExecutableAsk("remove_token", args);
+                grabbedObject.SetActive(false);
+                score = score + 1;
+                updateHUD();
+               // toDelete.Add(grabbedObject);
+        }
+        
+    }
 }
