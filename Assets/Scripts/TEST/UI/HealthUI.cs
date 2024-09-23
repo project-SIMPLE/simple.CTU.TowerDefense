99e8043d4cabd8ebed62acc37c3b8cea3cfbb485
o0WhiteFox0o <son.nguyenhong1990@gmail.com>
2024-09-23 21:39:57 +0700

    Update Animation
    Animation fix

--- Assets/Scripts/TEST/UI/HealthUI.cs
+++ Assets/Scripts/TEST/UI/HealthUI.cs
@@ -8,18 +8,37 @@ public class HealthUI : MonoBehaviour
 {
     public TextMeshProUGUI hp;
     IDamageable damageable;
+    public Image enemy_UI_1;
+    public Image enemy_UI_2;
+    public Image enemy_UI_Clean;
     
     // Start is called before the first frame update
     void Start()
     {
        damageable = GetComponent<IDamageable>();
+       enemy_UI_1.enabled = true;
+       enemy_UI_2.enabled = false;
+       enemy_UI_Clean.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         int health = damageable.Health;
-        if (health <= 0) hp.text = "0";
+        if (health <= 0) 
+        {
+            enemy_UI_1.enabled = false;
+            enemy_UI_2.enabled = false;
+            enemy_UI_Clean.enabled = true;
+            hp.text = "0";
+        }
+        else if(health ==1)
+        {
+            enemy_UI_1.enabled = false;
+            enemy_UI_2.enabled = true;
+            enemy_UI_Clean.enabled = false;
+            hp.text = "1";
+        }
         else hp.text = health.ToString();
     }
 }
